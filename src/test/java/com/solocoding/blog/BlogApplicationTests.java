1e5cfe6463727dfd8be32b2c9b2e5d020d342976
Vito Millozza <v.millozza@gmail.com>
2023-08-28 08:43:53 +0200

    Starting

---
+++ src/test/java/com/solocoding/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solocoding.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
