3bfcf2971fee5d5a09b75cd35ac42458b37527d6
Vito Millozza <v.millozza@gmail.com>
2023-08-28 11:22:08 +0200

    Create comment

--- src/main/resources/db/migration/V2__Create_comment_table.sql
+++ src/main/resources/db/migration/V2__Create_comment_table.sql
@@ -1,9 +1,10 @@
 -- V1__Create_post_table.sql
-CREATE TABLE post (
+CREATE TABLE comment (
     id SERIAL PRIMARY KEY,
     comment VARCHAR(256) NOT NULL,
-    comment_date timestamp ,
-    deleted smallint,
-    commenter_id integer
-
+    comment_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    commenter_id INTEGER
+    -- Se esiste una tabella 'users', potresti voler aggiungere un vincolo di chiave esterna come questo:
+    -- FOREIGN KEY (commenter_id) REFERENCES users(id)
 );
